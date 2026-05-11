dc3c4e8d8fda57509c6f0e54dd273a029ec05614
Maria-Adriana Ciurila <maria-adriana.ciurila@student.tuiasi.ro>
2026-05-11 21:58:32 +0300

    Design changes

--- Proiect/Form1.Designer.cs
+++ Proiect/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.labelLinesOfCode = new System.Windows.Forms.Label();
             this.buttonWriteCode = new System.Windows.Forms.Button();
             this.buttonHireJunior = new System.Windows.Forms.Button();
@@ -45,34 +46,39 @@
             // labelLinesOfCode
             // 
             this.labelLinesOfCode.AutoSize = true;
-            this.labelLinesOfCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelLinesOfCode.Location = new System.Drawing.Point(39, 218);
+            this.labelLinesOfCode.Cursor = System.Windows.Forms.Cursors.Help;
+            this.labelLinesOfCode.Font = new System.Drawing.Font("Consolas", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelLinesOfCode.Location = new System.Drawing.Point(271, 74);
             this.labelLinesOfCode.Name = "labelLinesOfCode";
-            this.labelLinesOfCode.Size = new System.Drawing.Size(124, 20);
+            this.labelLinesOfCode.Size = new System.Drawing.Size(170, 22);
             this.labelLinesOfCode.TabIndex = 0;
             this.labelLinesOfCode.Text = "Lines of Code: 0";
             this.labelLinesOfCode.Click += new System.EventHandler(this.labelLinesOfCode_Click);
             // 
             // buttonWriteCode
             // 
-            this.buttonWriteCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonWriteCode.Location = new System.Drawing.Point(22, 181);
+            this.buttonWriteCode.BackColor = System.Drawing.Color.Green;
+            this.buttonWriteCode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonWriteCode.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonWriteCode.Font = new System.Drawing.Font("Consolas", 13.74545F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonWriteCode.Location = new System.Drawing.Point(57, 74);
             this.buttonWriteCode.Name = "buttonWriteCode";
-            this.buttonWriteCode.Size = new System.Drawing.Size(158, 34);
+            this.buttonWriteCode.Size = new System.Drawing.Size(183, 88);
             this.buttonWriteCode.TabIndex = 1;
             this.buttonWriteCode.Text = "Write Code";
-            this.buttonWriteCode.UseVisualStyleBackColor = true;
+            this.buttonWriteCode.UseVisualStyleBackColor = false;
             this.buttonWriteCode.Click += new System.EventHandler(this.buttonWriteCode_Click);
             // 
             // buttonHireJunior
             // 
-            this.buttonHireJunior.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonHireJunior.Location = new System.Drawing.Point(507, 117);
+            this.buttonHireJunior.BackColor = System.Drawing.Color.Transparent;
+            this.buttonHireJunior.Font = new System.Drawing.Font("Consolas", 13.74545F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonHireJunior.Location = new System.Drawing.Point(532, 254);
             this.buttonHireJunior.Name = "buttonHireJunior";
-            this.buttonHireJunior.Size = new System.Drawing.Size(273, 34);
+            this.buttonHireJunior.Size = new System.Drawing.Size(199, 62);
             this.buttonHireJunior.TabIndex = 2;
             this.buttonHireJunior.Text = "Hire Junior Dev (Cost: 50)";
-            this.buttonHireJunior.UseVisualStyleBackColor = true;
+            this.buttonHireJunior.UseVisualStyleBackColor = false;
             this.buttonHireJunior.Click += new System.EventHandler(this.buttonHireJunior_Click);
             // 
             // gameTimer
@@ -83,13 +89,14 @@
             // 
             // buttonHireSenior
             // 
-            this.buttonHireSenior.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonHireSenior.Location = new System.Drawing.Point(507, 167);
+            this.buttonHireSenior.BackColor = System.Drawing.Color.Transparent;
+            this.buttonHireSenior.Font = new System.Drawing.Font("Consolas", 13.74545F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonHireSenior.Location = new System.Drawing.Point(532, 340);
             this.buttonHireSenior.Name = "buttonHireSenior";
-            this.buttonHireSenior.Size = new System.Drawing.Size(273, 34);
+            this.buttonHireSenior.Size = new System.Drawing.Size(199, 62);
             this.buttonHireSenior.TabIndex = 3;
             this.buttonHireSenior.Text = "Hire Senior Dev (Cost: 500)";
-            this.buttonHireSenior.UseVisualStyleBackColor = true;
+            this.buttonHireSenior.UseVisualStyleBackColor = false;
             this.buttonHireSenior.Click += new System.EventHandler(this.buttonHireSenior_Click);
             // 
             // animationTimer
@@ -99,54 +106,59 @@
             // 
             // buttonBuyKeyboard
             // 
-            this.buttonBuyKeyboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonBuyKeyboard.Location = new System.Drawing.Point(257, 117);
+            this.buttonBuyKeyboard.BackColor = System.Drawing.Color.Transparent;
+            this.buttonBuyKeyboard.Font = new System.Drawing.Font("Consolas", 13.74545F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonBuyKeyboard.Location = new System.Drawing.Point(69, 254);
             this.buttonBuyKeyboard.Name = "buttonBuyKeyboard";
-            this.buttonBuyKeyboard.Size = new System.Drawing.Size(233, 48);
+            this.buttonBuyKeyboard.Size = new System.Drawing.Size(297, 62);
             this.buttonBuyKeyboard.TabIndex = 4;
-            this.buttonBuyKeyboard.Text = "Buy Mechanical Keyboard \r\n(Cost: 250)";
-            this.buttonBuyKeyboard.UseVisualStyleBackColor = true;
+            this.buttonBuyKeyboard.Text = "Buy Mechanical Keyboard\r\n(Cost: 250)";
+            this.buttonBuyKeyboard.UseVisualStyleBackColor = false;
             this.buttonBuyKeyboard.Click += new System.EventHandler(this.buttonBuyKeyboard_Click);
             // 
             // labelPermanent
             // 
             this.labelPermanent.AutoSize = true;
-            this.labelPermanent.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelPermanent.Location = new System.Drawing.Point(278, 80);
+            this.labelPermanent.Font = new System.Drawing.Font("Consolas", 15.70909F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPermanent.Location = new System.Drawing.Point(95, 207);
             this.labelPermanent.Name = "labelPermanent";
-            this.labelPermanent.Size = new System.Drawing.Size(189, 24);
+            this.labelPermanent.Size = new System.Drawing.Size(246, 28);
             this.labelPermanent.TabIndex = 5;
             this.labelPermanent.Text = "Permanent Upgrades";
             // 
             // labelTeam
             // 
             this.labelTeam.AutoSize = true;
-            this.labelTeam.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTeam.Location = new System.Drawing.Point(574, 80);
+            this.labelTeam.Font = new System.Drawing.Font("Consolas", 15.70909F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTeam.Location = new System.Drawing.Point(539, 207);
             this.labelTeam.Name = "labelTeam";
-            this.labelTeam.Size = new System.Drawing.Size(146, 24);
+            this.labelTeam.Size = new System.Drawing.Size(181, 28);
             this.labelTeam.TabIndex = 6;
             this.labelTeam.Text = "Team Upgrades";
             // 
             // buttonReleaseVersion
             // 
-            this.buttonReleaseVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonReleaseVersion.Location = new System.Drawing.Point(12, 12);
+            this.buttonReleaseVersion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(191)))), ((int)(((byte)(63)))));
+            this.buttonReleaseVersion.Font = new System.Drawing.Font("Consolas", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonReleaseVersion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(13)))), ((int)(((byte)(6)))));
+            this.buttonReleaseVersion.Location = new System.Drawing.Point(10, 12);
             this.buttonReleaseVersion.Name = "buttonReleaseVersion";
             this.buttonReleaseVersion.Size = new System.Drawing.Size(776, 32);
             this.buttonReleaseVersion.TabIndex = 7;
             this.buttonReleaseVersion.Text = "Release Version";
-            this.buttonReleaseVersion.UseVisualStyleBackColor = true;
+            this.buttonReleaseVersion.UseVisualStyleBackColor = false;
             this.buttonReleaseVersion.Click += new System.EventHandler(this.buttonReleaseVersion_Click);
             // 
             // buttonBug
             // 
-            this.buttonBug.BackColor = System.Drawing.Color.Red;
-            this.buttonBug.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBug.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(44)))));
+            this.buttonBug.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonBug.BackgroundImage")));
+            this.buttonBug.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.buttonBug.Font = new System.Drawing.Font("Courier New", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.buttonBug.ForeColor = System.Drawing.Color.White;
-            this.buttonBug.Location = new System.Drawing.Point(350, 245);
+            this.buttonBug.Location = new System.Drawing.Point(490, 74);
             this.buttonBug.Name = "buttonBug";
-            this.buttonBug.Size = new System.Drawing.Size(219, 58);
+            this.buttonBug.Size = new System.Drawing.Size(267, 116);
             this.buttonBug.TabIndex = 8;
             this.buttonBug.Text = "BUG";
             this.buttonBug.UseVisualStyleBackColor = false;
@@ -157,7 +169,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(13)))), ((int)(((byte)(6)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(798, 635);
             this.Controls.Add(this.buttonBug);
             this.Controls.Add(this.buttonReleaseVersion);
             this.Controls.Add(this.labelTeam);
@@ -167,8 +181,11 @@
             this.Controls.Add(this.buttonHireJunior);
             this.Controls.Add(this.buttonWriteCode);
             this.Controls.Add(this.labelLinesOfCode);
+            this.Font = new System.Drawing.Font("Consolas", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(191)))), ((int)(((byte)(63)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Code Clicker";
             this.ResumeLayout(false);
             this.PerformLayout();
 
